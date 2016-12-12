7c0db39d50080cabc0c0ffc6952c46025c127e35
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2016-12-12 23:06:16 +0100

    Automatically open URLs in browser, resolves #20

--- electron/main.js
+++ electron/main.js
@@ -29,7 +29,7 @@ function createWindow() {
     // mainWindow.webContents.openDevTools()
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
+    mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
@@ -49,7 +49,7 @@ function createWindow() {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
@@ -57,7 +57,7 @@ app.on('window-all-closed', function() {
     }
 })
 
-app.on('activate', function() {
+app.on('activate', function () {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
@@ -102,6 +102,10 @@ express.ws('/', (ws, req) => {
                 if (settings.endCharacter) {
                     robot.keyTap(settings.endCharacter);
                 }
+            }
+
+            if (settings.enableOpenInBrowser) {
+                shell.openExternal(message.data.scannings[0].text);
             }
         }
     });
--- src/app/models/settings.model.ts
+++ src/app/models/settings.model.ts
@@ -1,5 +1,6 @@
 export class SettingsModel {
     enableRealtimeStrokes: boolean = true;
+    enableOpenInBrowser: boolean = false;
     endCharacter: string = 'enter';
     newLineCharacter: string  = 'CRLF';
     enableQuotes: boolean = true;
