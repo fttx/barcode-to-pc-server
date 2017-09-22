92a082b69b082244bb852bc11f2b3359e3d72b40
filippo <filippo.tortomasi@gmail.com>
2017-09-22 23:19:30 +0200

    Updated dependencies and angular project files

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { FooPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('foo App', () => {
-  let page: FooPage;
+describe('asd App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new FooPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
