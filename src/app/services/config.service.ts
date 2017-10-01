218265e77c0379ca703a5b76b52055682917914a
filippo <filippo.tortomasi@gmail.com>
2017-10-01 21:58:40 +0200

    Fix faq url

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -7,7 +7,7 @@ export class ConfigService {
 
   public static URL_WEBSITE = 'https://barcodetopc.com/';
   public static URL_DOWNLOAD = 'https://barcodetopc.com/#download';
-  public static URL_FAQ = 'https://barcodetopc.com/faq';
+  public static URL_FAQ = 'https://barcodetopc.com/faq.html';
   public static URL_API = "https://barcodetopc.com/http-api.json"
   public static URL_GITHUB_SERVER = 'https://github.com/fttx/barcode-to-pc-server';
   public static URL_GITHUB_APP = 'https://github.com/fttx/barcode-to-pc-app';
