f34c9be18ba008683080f94411d2dfefb5ff9fb9
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2017-12-01 11:15:25 +0100

    Fix certificate file name

--- macos-signing.sh
+++ macos-signing.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl $CSC_LINK -o outfile
+curl $CSC_LINK -o certificate.p12
 
 # openssl aes-256-cbc -k "$OPENSSL_PASSWORD" -in InstallerAndExecutableCertificates.encr.p12 -d -a -out InstallerAndExecutableCertificates.encr.p12
 
@@ -14,7 +14,7 @@ security unlock-keychain -p travis $KEY_CHAIN_NAME
 security set-keychain-settings -t 3600 -u $KEY_CHAIN_NAME
 
 # Add certificates to keychain and allow codesign to access them
-security import ./InstallerAndExecutableCertificates.p12 -k ~/Library/Keychains/$KEY_CHAIN_NAME -P $CSC_KEY_PASSWORD
+security import certificate.p12 -k ~/Library/Keychains/$KEY_CHAIN_NAME -P $CSC_KEY_PASSWORD
 
 echo "Add keychain to keychain-list"
 security list-keychains -s $KEY_CHAIN_NAME
