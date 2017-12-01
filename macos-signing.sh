9fed90ac058b85bbc9377acaa64a94fcb850ffea
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2017-12-01 09:26:17 +0100

    Fix macOS signing

--- macos-signing.sh
+++ macos-signing.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
-curl https://www.dropbox.com/s/qs6lz1dd9teh76g/InstallerAndExecutableCertificates.encr.p12?dl=1 -o outfile
+curl $CSC_LINK -o outfile
 
-openssl aes-256-cbc -k "$OPENSSL_PASSWORD" -in InstallerAndExecutableCertificates.encr.p12 -d -a -out InstallerAndExecutableCertificates.encr.p12
+# openssl aes-256-cbc -k "$OPENSSL_PASSWORD" -in InstallerAndExecutableCertificates.encr.p12 -d -a -out InstallerAndExecutableCertificates.encr.p12
 
-
-KEY_CHAIN=mac-build.keychain
-security create-keychain -p travis $KEY_CHAIN
+KEY_CHAIN_NAME=mac-build.keychain
+security create-keychain -p travis $KEY_CHAIN_NAME
 # Make the keychain the default so identities are found
-security default-keychain -s $KEY_CHAIN
+security default-keychain -s $KEY_CHAIN_NAME
 # Unlock the keychain
-security unlock-keychain -p travis $KEY_CHAIN
+security unlock-keychain -p travis $KEY_CHAIN_NAME
 # Set keychain locking timeout to 3600 seconds
-security set-keychain-settings -t 3600 -u $KEY_CHAIN
+security set-keychain-settings -t 3600 -u $KEY_CHAIN_NAME
 
 # Add certificates to keychain and allow codesign to access them
-security import ./InstallerAndExecutableCertificates.encr.p12 -k ~/Library/Keychains/$KEY_CHAIN -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./InstallerAndExecutableCertificates.p12 -k ~/Library/Keychains/$KEY_CHAIN_NAME -P $CSC_KEY_PASSWORD
 
 echo "Add keychain to keychain-list"
-security list-keychains -s $KEY_CHAIN
+security list-keychains -s $KEY_CHAIN_NAME
 
 echo "Settting key partition list"
-security set-key-partition-list -S apple-tool:,apple: -s -k travis $KEY_CHAIN
+security set-key-partition-list -S apple-tool:,apple: -s -k travis $KEY_CHAIN_NAME
 
 exit 0;
