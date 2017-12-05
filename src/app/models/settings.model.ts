3b7f6c4b11a0c825c69a6ac102699613cb769afb
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2017-12-05 11:05:03 +0100

    Fix new line character x2

--- src/app/models/settings.model.ts
+++ src/app/models/settings.model.ts
@@ -8,6 +8,6 @@ export class SettingsModel {
         { name: 'BARCODE', value: 'barcode', type: 'barcode' },
         { name: 'ENTER', value: 'enter', type: 'key' }
     ];
-    newLineCharacter: string = os.EOL.replace('\r', 'CR').replace('\n', 'LF');;
+    newLineCharacter: string = os.release().toLowerCase().indexOf('windows') == -1 ? 'LF' : 'CRLF';
     enableQuotes: boolean = true;
 }
