811b34d01094f5cef2ccfead6912209ad500ef2a
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2018-01-27 11:39:31 +0100

    Fix animation #90

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
-import { ClipboardModule } from 'ngx-clipboard';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 import { QRCodeModule } from 'angular2-qrcode';
+import { DragulaModule } from 'ng2-dragula';
+import { ModalModule, TooltipModule } from 'ngx-bootstrap';
+import { ClipboardModule } from 'ngx-clipboard';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './pages/main/main.component';
-
-
-import { ElectronService } from './services/electron.service';
-import { Storage } from './services/storage.service';
-import { EverConnectedService } from './services/ever-connected.service';
-import { UtilsService } from './services/utils.service';
-import { ScanSessionsComponent } from './components/scan-sessions/scan-sessions.component';
-import { ScanSessionComponent } from './components/scan-session/scan-session.component';
 import { CircleTextComponent } from './components/circle-text/circle-text.component';
+import { ScanSessionComponent } from './components/scan-session/scan-session.component';
+import { ScanSessionsComponent } from './components/scan-sessions/scan-sessions.component';
 import { StringComponentComponent } from './components/string-component/string-component.component';
-
-import { ModalModule, TooltipModule } from 'ngx-bootstrap';
-import { DragulaModule } from 'ng2-dragula'
-
+import { MainComponent } from './pages/main/main.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { ElectronService } from './services/electron.service';
+import { EverConnectedService } from './services/ever-connected.service';
+import { Storage } from './services/storage.service';
+import { UtilsService } from './services/utils.service';
 
 
 const routes: Routes = [
@@ -56,7 +52,8 @@ const routes: Routes = [
         TooltipModule.forRoot(),
         FormsModule,
         QRCodeModule,
-        DragulaModule
+        DragulaModule,
+        BrowserAnimationsModule
     ],
     providers: [
         ElectronService,
