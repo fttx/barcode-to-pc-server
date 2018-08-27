414da047196a892b0ef5a721dea0e9f8b3ae5c5e
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2018-08-27 21:52:11 +0200

    Fix appveyor x2

--- ionic/gulpfile.js
+++ ionic/gulpfile.js
@@ -1,18 +1,19 @@
 const gulp = require('gulp')
-const config = require('@ionic/app-scripts/dist/util/config')
-const ionic = require('@ionic/app-scripts')
 const { exec, execSync, spawn } = require('child_process');
 const electronBuilder = require('../node_modules/electron-builder')
 const typescript = require('../node_modules/typescript')
 const mocha = require('../node_modules/mocha')
-const  fs = require('fs');
+const fs = require('fs');
 
 gulp.task('serve', ['electron:assets'], () => {
+  const config = require('@ionic/app-scripts/dist/util/config')
+  const ionic = require('@ionic/app-scripts')
+
   exec('npm run tsc:watch-electron', { cwd: '../', stdio: "inherit", shell: true })
   let buildContext = config.generateContext();
   ionic.addArgv('--nobrowser')
   ionic.addArgv('--port')
-  ionic.addArgv('8200')  
+  ionic.addArgv('8200')
   ionic.serve(buildContext).then(() => {
     exec('npm run electron:dev', { cwd: '../', stdio: "inherit", shell: true })
   }).catch(() => {
@@ -20,51 +21,67 @@ gulp.task('serve', ['electron:assets'], () => {
   });
 })
 
-gulp.task('build', ['electron:assets'], () => {
-  return Promise.all([ // promises are executed asynchronously
-    new Promise((resolve, reject) => {
-      gulp.src(['../electron/electron-resources/**/*'])
-        .pipe(gulp.dest('../dist/build/'))
-        .on('error', reject)
-        .on('end', resolve)
-    }),
-    new Promise((resolve, reject) => {
-      execSync('npm run tsc:electron', { cwd: '../', stdio: "inherit", shell: true })
-      gulp.src(['../package.json'])
-        .pipe(gulp.dest('../dist/'))
-        .on('end', resolve)
-        .on('error', reject);
-    }),
-    new Promise((resolve, reject) => {
-      // ionic.build doesn't add the platform if it isn't installed first.
-      // let buildContext = config.generateContext({
-      //   isProd: true,
-      //   platform: 'browser',
-      // });
-      // ionic.build(buildContext).then(() => {
-      //   gulp
-      //     .src(['./platforms/browser/www/**/*'])
-      //     .pipe(gulp.dest('../dist/ionic/www/'))
-      //     .on('error', reject)
-      //     .on('end', resolve)
-      // })
+gulp.task('electron:resources', ['mkdir'], () => {
+  return new Promise((resolve, reject) => {
+    gulp.src(['../electron/electron-resources/**/*'])
+      .pipe(gulp.dest('../dist/build/'))
+      .on('error', reject)
+      .on('end', resolve)
+  })
+});
 
-      // exec is less cross-platoform but at least works
-      execSync('ionic cordova build browser --prod', { stdio: "inherit", shell: true })
-      gulp
-        .src(['./platforms/browser/www/**/*'])
-        .pipe(gulp.dest('../dist/ionic/www/'))
-        .on('error', reject)
-        .on('end', resolve)
-    }),
-  ])  // a similar behavior can be achieved by creating multiple gulp.task which do not return anything
-})
+gulp.task('electron:tsc', ['mkdir'], () => {
+  return new Promise((resolve, reject) => {
+    execSync('npm run tsc:electron', { cwd: '../', stdio: "inherit", shell: true })
+    gulp.src(['../package.json'])
+      .pipe(gulp.dest('../dist/'))
+      .on('end', resolve)
+      .on('error', reject);
+  })
+});
 
-gulp.task('dist', ['build', 'dist:install-prod'], () => {
+gulp.task('ionic:install', ['mkdir'], () => {
+  return new Promise((resolve, reject) => {
+    // ionic.build doesn't add the platform if it isn't installed first.
+    // let buildContext = config.generateContext({
+    //   isProd: true,
+    //   platform: 'browser',
+    // });
+    // ionic.build(buildContext).then(() => {
+    //   gulp
+    //     .src(['./platforms/browser/www/**/*'])
+    //     .pipe(gulp.dest('../dist/ionic/www/'))
+    //     .on('error', reject)
+    //     .on('end', resolve)
+    // })
+
+    if (!fs.existsSync('node_modules') || !fs.existsSync('platforms/browser')) {
+      execSync('npm i && ionic cordova platform add browser', { stdio: "inherit", shell: true })
+    }
+    // exec is less cross-platoform but at least works
+    execSync('ionic cordova build browser --prod', { stdio: "inherit", shell: true })
+    gulp
+      .src(['./platforms/browser/www/**/*'])
+      .pipe(gulp.dest('../dist/ionic/www/'))
+      .on('error', reject)
+      .on('end', resolve)
+  })
+});
+
+gulp.task('electron:assets', ['mkdir'], () => {
+  return gulp // when this task is used as dependency from another task it will wait for this task to complete
+    .src(['../electron/src/assets/**/*'])
+    .pipe(gulp.dest('../dist/electron/src/assets/'))
+});
+
+
+gulp.task('build', ['electron:resources', 'electron:tsc', 'ionic:install', 'electron:assets']);
+
+gulp.task('dist', ['build'], () => {
   return electronBuilder.build({ projectDir: '../dist' });
 })
 
-gulp.task('publish', ['build', 'dist:install-prod'], () => {
+gulp.task('publish', ['build'], () => {
   return electronBuilder.build({ projectDir: '../dist', publish: 'always' });
 })
 
@@ -73,15 +90,12 @@ gulp.task('test', ['build'], () => {
   return execSync('npm run mocha', { cwd: '../', stdio: "inherit", shell: true })
 })
 
-gulp.task('electron:assets', () => {
-  return gulp // when this task is used as dependency from another task it will wait for this task to complete
-    .src(['../electron/src/assets/**/*'])
-    .pipe(gulp.dest('../dist/electron/src/assets/'))
-});
 
-gulp.task('dist:install-prod', () => {
-  if (!fs.existsSync('../dist')) {
-    fs.mkdirSync('../dist');
-  }
-  return execSync('npm install --prod --ignore-scripts', { cwd: '../dist', stdio: "inherit", shell: true })
+gulp.task('mkdir', () => {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync('../dist')) {
+      fs.mkdirSync('../dist');
+    }
+    resolve();
+  })
 })
