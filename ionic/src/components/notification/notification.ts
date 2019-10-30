93503a0a1d33aaa8bf59955924325a1fbe5ce091
filippo <filippo.tortomasi@gmail.com>
2019-10-30 12:08:26 +0100

    Prevent notification from showing multiple times

--- ionic/src/components/notification/notification.ts
+++ ionic/src/components/notification/notification.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ElectronProvider } from '../../providers/electron/electron';
+import ElectronStore from 'electron-store';
 
 /**
- * Generated class for the NotificationComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
+ * Generates a box in the bottom-right side of the screen.
+ * Once the notification is dismissed, or the CTA button is clicked, the notification won't be shown again
+ * Required Input parameters: id
  */
 @Component({
   selector: 'notification',
@@ -15,14 +15,16 @@ export class NotificationComponent {
   @Input('title') title: string;
   @Input('hideAfter') hideAfter: number;
   @Input('cta') cta: string;
+  @Input('id') id: string;
 
-  public hidden = false;
+  public hidden = true;
+
+  private store: ElectronStore;
 
   constructor(
     private electronProvider: ElectronProvider,
   ) {
-    console.log('Hello NotificationComponent Component');
-    this.title = 'Hello World';
+    this.store = new this.electronProvider.ElectronStore();
   }
 
   ngOnInit() {
@@ -30,6 +32,11 @@ export class NotificationComponent {
       setTimeout(() => {
         this.hidden = true;
       }, this.hideAfter)
+    }
+
+    let alreadyViewed = this.store.get(this.getStoreShownKey(), false);
+    if (!alreadyViewed) {
+      this.show();
     }
   }
 
@@ -39,9 +46,15 @@ export class NotificationComponent {
 
   hide() {
     this.hidden = true;
+    this.store.set(this.getStoreShownKey(), true);
   }
 
   onCtaClick() {
     this.electronProvider.shell.openExternal(this.cta);
+    this.hide();
+  }
+
+  private getStoreShownKey() {
+    return 'notification_' + this.id + '_viewed';
   }
 }
