aca8e5d3d14e4b0f9de7a04549565c585f7a7263
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2020-04-05 12:25:12 +0200

    Fix start minimized issue (untested) #241

--- electron/src/handlers/ui.handler.ts
+++ electron/src/handlers/ui.handler.ts
@@ -153,11 +153,6 @@ export class UiHandler implements Handler {
             this.mainWindow.loadURL(_path.join('file://', __dirname, '../../../ionic/www/index.html'));
         }
 
-        let loginItemSettings = app.getLoginItemSettings();
-        if (loginItemSettings.wasOpenedAsHidden && loginItemSettings.wasOpenedAtLogin) {
-            this.mainWindow.emit('close')
-        }
-
         if (process.platform === 'darwin') {
             let template: MenuItemConstructorOptions[] = [
                 {
@@ -288,6 +283,11 @@ export class UiHandler implements Handler {
                 selectionMenu.popup({ window: this.mainWindow });
             }
         })
+
+        let loginItemSettings = app.getLoginItemSettings();
+        if (loginItemSettings.wasOpenedAsHidden && loginItemSettings.wasOpenedAtLogin) {
+            this.mainWindow.emit('close')
+        }
     }
 
     onWsMessage(ws: WebSocket, message: any, req: http.IncomingMessage) {
