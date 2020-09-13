3848055bdba089c081791c5a40e37c43f6e2f2d9
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2020-09-13 19:29:07 +0200

    Remove ambiguous variable

--- ionic/src/app/app.component.ts
+++ ionic/src/app/app.component.ts
@@ -122,13 +122,12 @@ export class MyApp {
       // Used to open files (double click)
       // On Windows when double-clicking, the system passes the file path
       // of the clicked file to the main exectutable.
-      let process = this.electronProvider.remote.process;
       let checkArgv = (argv) => {
         if (argv.length >= 2) { // process.platform == 'win32' &&
           this.events.publish('import_btpt', argv[1])
         }
       }
-      checkArgv(process.argv);
+      checkArgv(this.electronProvider.process.argv);
       this.electronProvider.ipcRenderer.on('second-instance-open', (event, argv) => {
         checkArgv(argv)
       })
