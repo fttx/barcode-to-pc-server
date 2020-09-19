2971959b4ed3f1c81a27edd8a41a486cb05e0625
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2020-09-19 09:17:42 +0200

    Update model

--- ionic/src/models/output-profile.model.ts
+++ ionic/src/models/output-profile.model.ts
@@ -30,10 +30,44 @@ export class OutputProfileModel {
     static ContainsBlockingComponents(outputProfile: OutputProfileModel): boolean {
         return outputProfile.outputBlocks.findIndex(x =>
             x.value == 'number' || x.value == 'text' || x.type == 'select_option' ||
+            x.type == 'http' || x.type == 'run' || x.type == 'csv_lookup' ||
             /**
              * @deprecated for update transition only
              */
             x.value == 'quantity'
         ) != -1;
+    }
+    /**
+     * @returns TRUE when there is at least one components component that has a
+     * different enabledFormats field from the other BARCODE components.
+     */
+    // static ContainsMixedBarcodeFormats(outputProfile: OutputProfileModel): boolean {
+    //   for (let i = 0; i < outputProfile.outputBlocks.length; i++) {
+    //     let block1 = outputProfile.outputBlocks[i];
+    //     if (block1.type == 'barcode') {
+    //       for (let j = i + 1; j < outputProfile.outputBlocks.length; j++) {
+    //         let block2 = outputProfile.outputBlocks[j];
+    //         if (block2.type == 'barcode' && JSON.stringify(block2.enabledFormats) != JSON.stringify(block1.enabledFormats)) {
+    //           return true;
+    //         }
+    //       }
+    //     }
+    //   }
+    //   return false;
+    // }
+    /**
+     * @returns TRUE when there is at least two BARCODE components.
+     */
+    static ContainsMultipleBarcodeFormats(outputProfile: OutputProfileModel): boolean {
+        let count = 0;
+        for (let i = 0; i < outputProfile.outputBlocks.length; i++) {
+            if (outputProfile.outputBlocks[i].type == 'barcode') {
+                count++;
+            }
+            if (count >= 2) {
+                return true;
+            }
+        }
+        return false;
     }
 }
