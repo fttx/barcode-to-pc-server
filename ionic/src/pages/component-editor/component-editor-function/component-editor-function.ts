4993e8e2cdd94147813e7d3539ebf7b22aad0d2d
Filippo <filippo@email.com>
2022-08-13 09:39:24 +0200

    Fix open docs URL issue

--- ionic/src/pages/component-editor/component-editor-function/component-editor-function.ts
+++ ionic/src/pages/component-editor/component-editor-function/component-editor-function.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular';
 import { OutputBlockModel } from '../../../models/output-block.model';
+import { ElectronProvider } from '../../../providers/electron/electron';
 
 @Component({
   selector: 'page-component-editor-function',
@@ -11,6 +12,7 @@ export class ComponentEditorFunctionPage {
   public outputBlock: OutputBlockModel;
   constructor(
     public navParams: NavParams,
+    public electronProvider: ElectronProvider, // template
   ) {
     this.outputBlock = this.navParams.get('outputBlock');
   }
