5cd91c33b0e77b7f5a37bcf323af021e25ef1a4e
Filippo <filippo.tortomasi@gmail.com>
2024-07-17 17:10:52 +0200

    feat: remove electron folder

--- electron/build/renameLicense.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rename 's/lproj_AboutBonjour.rtf/rtf/' *.rtf
-rename 's/Bonjour.Resources/license/' *.rtf
--- electron/src/utils.ts
+++
@@ -1,5 +0,0 @@
-export class Utils {
-    public static IsDev() {
-        return process.argv.slice(1).some(val => val === '--dev');
-    }
-}
